2873a573bf5109205f676c71e72deb55333c8cce
Remtori <lqv.remtori@gmail.com>
2020-03-26 00:15:27 +0700

    Seperate Core and Client log

--- Reme/src/Reme/Core/Log.cpp
+++ Reme/src/Reme/Core/Log.cpp
@@ -2,25 +2,31 @@
 #include "Reme/Core/Log.h"
 
 #include <spdlog/spdlog-inl.h>
-
-#ifdef _DEBUG
-#include "spdlog/sinks/stdout_color_sinks.h"
-#else
-#include "spdlog/sinks/basic_file_sink.h"
-#endif
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace Reme
 {
-	std::shared_ptr<spdlog::logger> Log::s_Logger;
+	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
+	std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
 
 	void Log::Init()
 	{
-		spdlog::set_pattern("%^[%T] %n [%L]: %v%$");
-#ifdef _DEBUG
-		s_Logger = spdlog::stdout_color_mt("Reme Engine");
-#else
-		s_Logger = spdlog::basic_logger_mt("Reme Engine", "log.txt");
-#endif
-		s_Logger->set_level(spdlog::level::trace);
+		std::vector<spdlog::sink_ptr> logSinks;
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Reme.log", true));
+
+		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+		s_CoreLogger = std::make_shared<spdlog::logger>("REME", logSinks.begin(), logSinks.end());
+		spdlog::register_logger(s_CoreLogger);
+		s_CoreLogger->set_level(spdlog::level::trace);
+		s_CoreLogger->flush_on(spdlog::level::trace);
+
+		s_ClientLogger = std::make_shared<spdlog::logger>("CLIENT", logSinks.begin(), logSinks.end());
+		spdlog::register_logger(s_ClientLogger);
+		s_ClientLogger->set_level(spdlog::level::trace);
+		s_ClientLogger->flush_on(spdlog::level::trace);
 	}
 }
--- Reme/src/Reme/Core/Log.h
+++ Reme/src/Reme/Core/Log.h
@@ -9,9 +9,11 @@ namespace Reme
 	{
 	public:
 		static void Init();
-		inline static std::shared_ptr<spdlog::logger>& GetLogger() { return s_Logger; }
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetLogger() { return s_ClientLogger; }
 	private:
-		static std::shared_ptr<spdlog::logger> s_Logger;
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
 	};
 }
 
@@ -24,6 +26,12 @@ namespace Reme
 #define LOG_ERROR(...)    ::Reme::Log::GetLogger()->error(__VA_ARGS__)
 #define LOG_CRITICAL(...) ::Reme::Log::GetLogger()->critical(__VA_ARGS__)
 
+#define CORE_LOG_TRACE(...)    ::Reme::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define CORE_LOG_INFO(...)     ::Reme::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define CORE_LOG_WARN(...)     ::Reme::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define CORE_LOG_ERROR(...)    ::Reme::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define CORE_LOG_CRITICAL(...) ::Reme::Log::GetCoreLogger()->critical(__VA_ARGS__)
+
 #else
 
 #define LOG_TRACE(...)    
@@ -31,5 +39,11 @@ namespace Reme
 #define LOG_WARN(...)     
 #define LOG_ERROR(...)    
 #define LOG_CRITICAL(...) 
+
+#define CORE_LOG_TRACE(...)    
+#define CORE_LOG_INFO(...)     
+#define CORE_LOG_WARN(...)     
+#define CORE_LOG_ERROR(...)    
+#define CORE_LOG_CRITICAL(...) 
 
 #endif
